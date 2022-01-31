43c5b9ffa6f2ecaa4bfb7848789664990ec1c2ef
alfaroqi <roqialfaroqi@gmail.com>
2022-02-01 00:29:39 +0700

    refresh_tokens enpoint

--- service-user/models/RefreshToken.js
+++ service-user/models/RefreshToken.js
@@ -16,12 +16,12 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
-      created_at: {
+      createdAt: {
         field: "created_at",
         type: DataTypes.DATE,
         allowNull: false,
       },
-      updated_at: {
+      updatedAt: {
         field: "updated_at",
         type: DataTypes.DATE,
         allowNull: false,
---
+++ service-user/routes/handler/refresh-tokens/create.js
@@ -0,0 +1,41 @@
+const { User, RefreshToken } = require("../../../models");
+const Validator = require("fastest-validator");
+const v = new Validator();
+
+module.exports = async (req, res) => {
+  const userID = req.body.user_id;
+  const refreshToken = req.body.refresh_token;
+
+  const schema = {
+    user_id: { type: "number", integer: true },
+    refresh_token: { type: "string", min: 1 },
+  };
+
+  const validate = v.validate(req.body, schema);
+  if (validate !== true) {
+    return res.status(400).json({
+      status: "error",
+      message: validate,
+    });
+  }
+
+  const user = await User.findByPk(userID);
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+
+  const createdRefreshToken = await RefreshToken.create({
+    token: refreshToken,
+    user_id: userID,
+  });
+
+  return res.status(201).json({
+    message: "Refresh token created",
+    data: {
+      id: createdRefreshToken.id,
+      token: createdRefreshToken.token,
+    },
+  });
+};
